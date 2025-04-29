79cf97622c899df432ce6b2d81637c5b88568869
VashuGupta-rsh <vashugupta03@gmail.com>
2025-04-29 06:57:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -93,6 +93,9 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
@@ -108,3 +111,7 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
 });
+
+
+
+
